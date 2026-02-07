f120e031e6780a860831ad76ff93c753f3a79482
Pvrolomx <pvrolo@gmail.com>
2026-02-07 16:00:42 -0600

    feat: public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'marejadas-v1';
+const URLS_TO_CACHE = ['/', '/playas'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+});
